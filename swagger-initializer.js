fd411b0d0f4c4a779a5c570ccfc9c9abebfeedd4
Halou7682 <w.h@goldwaterpl.com>
2025-12-30 08:32:44 +1100

    Add files via upload

---
+++ swagger-initializer.js
@@ -0,0 +1,12 @@
+window.onload = function() {
+  window.ui = SwaggerUIBundle({
+    url: "./openapi.yaml",
+    dom_id: '#swagger-ui',
+    presets: [
+      SwaggerUIBundle.presets.apis,
+      SwaggerUIStandalonePreset
+    ],
+    layout: "StandaloneLayout"
+  });
+};
+
